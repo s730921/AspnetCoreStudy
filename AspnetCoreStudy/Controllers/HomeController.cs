8983b84439982fbbdf9554cbe2b3075d7f9ceabb
여왕벌 <s730921@hotmail.com>
2019-07-12 14:25:36 +0900

    초기 커밋.

---
+++ AspnetCoreStudy/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspnetCoreStudy.Models;
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var firstUser = new User();
+            //firstUser.UserNo = 1;
+            //firstUser.UserName = "홍길동";
+            var hongUser = new User()
+            {
+                UserNo = 1,
+                UserName = "홍길동"
+            };
+
+            // # 1번째 방식 View(Model);
+            //return View(hongUser);
+
+            // # 2번째 방식 ViewBag
+            //ViewBag.User = hongUser;
+            //return View();
+
+            // # 3번째 방식 ViewData
+            ViewData["UserNo"] = hongUser.UserNo;
+            ViewData["UserName"] = hongUser.UserName;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
